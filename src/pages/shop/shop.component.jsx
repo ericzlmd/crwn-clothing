d14ebdd560685e61e5336b522342d8c33b1ac415
ericzlmd <ericzlmd@gmail.com>
2020-02-10 13:22:29 -0500

    Uncommented local images path, and use cloud stored images

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -9,20 +9,18 @@ class ShopPage extends React.Component {
 
     this.state = {
       collections: SHOP_DATA
-    }
+    };
   }
 
   render() {
     const { collections } = this.state;
     return (
       <div className="shop-page">
-        {
-          collections.map(({id, ...otherCollections }) => (
-            <CollectionPreview key={id} {...otherCollections} />
-          ))
-        }
+        {collections.map(({ id, ...otherCollections }) => (
+          <CollectionPreview key={id} {...otherCollections} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
